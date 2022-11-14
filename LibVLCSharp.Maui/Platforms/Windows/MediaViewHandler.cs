4626ad682e47a3b8427c64685b2cbf467637437c
ChisterWu <wufeng4728704@hotmail.com>
2022-11-14 18:57:40 +0800

    add

--- LibVLCSharp.Maui/Platforms/Windows/MediaViewHandler.cs
+++ LibVLCSharp.Maui/Platforms/Windows/MediaViewHandler.cs
@@ -5,11 +5,11 @@ using Microsoft.Maui.Handlers;
 namespace LibVLCSharp.Maui;
 
 // All the code in this file is only included on Windows.
-public partial class MediaViewHandler : ViewHandler<IMediaView, VideoView>
+public partial class MediaViewHandler : ViewHandler<IMediaView, VideoViewx>
 {
-    protected override VideoView CreatePlatformView()
+    protected override VideoViewx CreatePlatformView()
     {
-        var view = new VideoView();
+        var view = new VideoViewx();
         view.Initialized += View_Initialized;
         return view;
     }
--- VLCSharp.Maui.Sample/MainPage.xaml.cs
+++ VLCSharp.Maui.Sample/MainPage.xaml.cs
@@ -17,10 +17,10 @@ public partial class MainPage : ContentPage
     [SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "<Pending>")]
     private void OnCounterClicked(object sender, EventArgs e)
 	{
-        _LibVLC = new();
+        _LibVLC = new(SwapChainOptions);
 
         VLC_MediaView.MediaPlayer = new LibVlcshared.MediaPlayer(_LibVLC);
-        var media = new LibVlcshared.Media(_LibVLC, new Uri("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"));
+        var media = new LibVlcshared.Media(_LibVLC, new Uri("https://tb-video.bdstatic.com/tieba-smallvideo-transcode-cae/14_85ebc1dea846ccb3c6dca9ffe3a88363_0_0_cae3.mp4?vt=0&pt=3&ver=&cr=2&cd=0&sid=&ft=2&tbau=2022-11-15_d532423a1e5fa98b5381181c3a51fdd90e71d0e0383e6f4b744d6dfce63e6809&ptid=8047416825"));
         VLC_MediaView.MediaPlayer.Play(media);
     }
 
